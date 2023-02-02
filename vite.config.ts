15cb357dae96a973065a1282acbc3baf22be1f55
Hasidic Dev <creekhippybear@gmail.com>
2023-02-02 17:52:52 -0500

    Redesigning site using tbs-5-alpha

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import type { UserConfig } from 'vite';
-
-const config: UserConfig = {
-	assetsInclude: ['**/*.glb', '**/*.fbx'],
-	plugins: [sveltekit()]
-};
-
-export default config;
